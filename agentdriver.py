6b928715cfa3b57f2db7e4228420ca067547f3d2
Bach Dániel <bd@ik.bme.hu>
2014-02-10 20:24:27 +0100

    stop celery on SIGINT

--- agentdriver.py
+++ agentdriver.py
@@ -3,11 +3,28 @@ from twisted.internet import reactor, inotify
 from twisted.python import filepath
 from agentcelery import celery
 from protocol import inotify_handler
-from os import getenv
+from os import getenv, listdir, path
 import logging
 
 
 SOCKET_DIR = getenv('SOCKET_DIR', '/var/lib/libvirt/serial')
+
+
+old_install_platform_tweaks = Worker.install_platform_tweaks
+def install_platform_tweaks(self, worker):
+    self.worker = worker
+    old_install_platform_tweaks(self, worker)
+Worker.install_platform_tweaks = install_platform_tweaks
+
+
+def reactor_started():
+    for f in listdir(SOCKET_DIR):
+        f = path.join(SOCKET_DIR, f)
+        inotify_handler(None, filepath.FilePath(f), None)
+
+
+def reactor_stopped(worker):
+    worker.worker.stop()
 
 
 def main():
@@ -19,6 +36,8 @@ def main():
     notifier.startReading()
     notifier.watch(filepath.FilePath(SOCKET_DIR),
                    callbacks=[inotify_handler])
+    reactor.callWhenRunning(reactor_started)
+    reactor.addSystemEventTrigger("before", "shutdown", reactor_stopped, w)
     reactor.run()
 
 if __name__ == '__main__':
